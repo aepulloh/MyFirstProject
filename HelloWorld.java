f6ab4b6a56b3a565d267f1e5db5f310d584fc5c0
vedlier <97926969+vedlier@users.noreply.github.com>
2022-01-18 09:52:39 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
   public static void main(){
     System.out.println("HelloWorld aep Kekkwww pulllllll");
+    System.out.println("HelloWorld aep lullllba");
     }
   }
       
